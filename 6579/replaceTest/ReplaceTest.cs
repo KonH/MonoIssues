dd501a6afc1b314fce1a573a9c6dd8fce36a2478
KonH <konh@yandex.ru>
2018-03-07 18:52:44 +0700

    Update run script, remove replaceTest;

--- 6579/replaceTest/ReplaceTest.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-public class Foo {
-	public static int Main(string[] args) {
-		Console.WriteLine(Array.IsNewAssembly);
-		return 0;
-	}
-}
--- 6579/run.sh
+++ 6579/run.sh
@@ -1,1 +1,1 @@
-mono Repro.exe
+MONO_PATH=/Users/konh/Git/mono/mcs/class/lib/net_4_x /Users/konh/Git/mono/mono/mini/mono Repro.exe
