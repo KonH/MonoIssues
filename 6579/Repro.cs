d3091e5cb8341a7232b00ab71586e978fd9bcce7
KonH <konh@yandex.ru>
2018-03-07 21:29:43 +0700

    Several test cases;

--- 6579/Repro.cs
+++ 6579/Repro.cs
@@ -3,7 +3,34 @@ using System.Collections.Generic;
 
 public class Foo {
 	public static int Main(string[] args) {
-		Console.WriteLine((typeof(Dictionary<string, object>).GetInterface("System.Collections.IDictionary", true) != null));
+		Console.WriteLine(
+			string.Format(
+				"Case #0: was: {0}, expected: {1}",
+				(typeof(Dictionary<string, object>).GetInterface("System.Collections.IDictionary", true) != null),
+				true
+			)
+		);
+		Console.WriteLine(
+			string.Format(
+				"Case #1: was: {0}, expected: {1}",
+				(typeof(Dictionary<string, object>).GetInterface("System.Collections.Idictionary", true) != null),
+				true
+			)
+		);
+		Console.WriteLine(
+			string.Format(
+				"Case #2: was: {0}, expected: {1}",
+				(typeof(Dictionary<string, object>).GetInterface("System.Collections.IDictionary", false) != null),
+				true
+			)
+		);
+		Console.WriteLine(
+			string.Format(
+				"Case #3: was: {0}, expected: {1}",
+				(typeof(Dictionary<string, object>).GetInterface("System.Collections.Idictionary", false) != null),
+				false
+			)
+		);
 		return 0;
 	}
 }
